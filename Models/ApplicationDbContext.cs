9c2e4f6570442b1a511f68d345c1b672883f8b43
arnob-das <contact.arnobdas@gmail.com>
2024-07-15 16:42:17 +0600

    database connected

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace messManagement.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
